f543f13bd746484d89d86ab4b1012b7bdd9a0c12
istekhar44 <67890istekharalam@gmail.com>
2025-07-13 15:37:25 +0530

    pattern 1

--- Loops/pattern.cpp
+++ Loops/pattern.cpp
@@ -1,9 +1,23 @@
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 int main(){
+    int n;
+    cin>>n;               // Pattern1
+    int i = 1;
+    while (i<=n){
+        int j = 0;
+        while(j<n){
+            cout<<"*";
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
     
-
 
    
 return 0;
-}
+}*/
+
+
+
