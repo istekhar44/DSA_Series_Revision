e972f3cc3633e983d1a0793c0f2b8754b11c79e8
istekhar44 <67890istekharalam@gmail.com>
2025-07-13 16:10:18 +0530

    pattern

--- Loops/pattern.cpp
+++ Loops/pattern.cpp
@@ -20,4 +20,38 @@ return 0;
 }*/
 
 
+/*#include <iostream>
+using namespace std;
+int main(){
+    int num ;
+    cin>>num;
+    cout<<"num"<<endl;
+    int row = 1;
+   while(row<=num){       //Best syntax of the patter
+    int col = 1;
+    while(col<=num){
+        cout<<"*";
+        col = col + 1;
+    }
+    cout<<endl;
+    row = row +1 ;
+
+   }
+}*/
+
+
+// Make the code of this pattern
+
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
+#include<iostream>
+using namespace std;
+int main(){
+    int num ;
+    
+
+   
+return 0;
+}
 
