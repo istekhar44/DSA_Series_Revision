bbd6f216de14e7b1bb0c3d168b60627c7429e48a
istekhar44 <67890istekharalam@gmail.com>
2025-07-13 15:19:46 +0530

    Pointer

---
+++ Pointers/BasicPointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   //Bacic of the Pointer
+   int num = 5;
+   int *ptr=&num;
+   cout<<*ptr<<endl;
+   cout<<ptr<<endl;
+   cout<<num<<endl;
+   cout<<&num<<endl;
+   
+return 0;
+}
