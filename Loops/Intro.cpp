9e07a9549fafd2176b6149ec6ebeee3f722da0ca
istekhar44 <67890istekharalam@gmail.com>
2025-07-13 12:27:51 +0530

    Pointers

---
+++ Loops/Intro.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main(){
+ string a= "hellow"  ;  //String is written in the "" double colons 
+ cout<< a <<endl;
+
+ char b = 'H' ;
+ cout<< b<<endl;    //Char is written in the single ' columns    
+ 
+
+ int c = 78;
+ cout<< c << endl;    //Integer 
+
+ double g = 4/5;
+ cout << g << endl;
+
+
+   //Relational Operator
+ // = Equal to
+ // > greater than 
+ // < Less than 
+ // != not equal to
+  
+  int x = 34;
+  int y = 67;
+  bool first= (x<y);     //Check the the integer which is grater
+  cout << first << endl;
+
+
+
+
+  //   && <- This is the (And ) operator use the multiple condition is necssary to true 
+  //   ||  <-  This is the (OR )  if one condition is true or  -> Ya fir
+  //   !a    <- make the true to false 
+
+  int h = 21;
+  cout << !h << endl;
+
+
+
+
+ 
+
+
+
+
+}
+
+
---
+++ Loops/loop.cpp
@@ -0,0 +1,117 @@
+/*#include <iostream>
+using namespace std;
+int main(){
+    int a = 34;
+    int b = 67;
+
+                // Use the multiple if else if
+    if(a>b){        
+        cout<<"print the A is Grater "<<endl;
+
+    }
+    else if (a<b){
+        cout<<"Print the B is Grater"<<endl;
+    }
+    else{
+        cout<<"Both are the Equal"<<endl;
+    }
+
+    return 0;
+
+}*/
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    //Print the counting
+    int n;                  //Print the counting
+    cin>>n;
+    int i = 0;
+    while(i<=n){
+        cout<<i << endl;
+        i +=1 ;
+    }
+
+return 0;
+}*/
+
+
+
+
+
+
+/*//Print the Sum of the n number
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i = 1;
+    int sum = 0;
+    while(i<=n){
+        sum = sum+i;
+        i=i+1;
+        
+
+    }
+    cout<<sum<<endl;
+
+   
+return 0;
+}*/
+
+
+
+/*//Print the sum of the even NO
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int sum;
+    sum = 0;
+    int i=1;
+    while(i<=n){
+        sum = sum +i;
+        i = i+2;
+    }
+
+    cout<<sum<<endl;
+
+
+   
+return 0;
+}*/
+
+
+
+
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;                    //Print this no is prime or not
+    int i = 2;
+    while(i<n){
+        if(n%i==0){
+            cout<<"this is NOt Prime"<<endl;
+
+        }
+        else {
+            cout<<"this is Prime "<<endl;
+        }
+
+        i=i+1; 
+        
+    }   
+return 0;
+}*/
+
+
+
+
+
