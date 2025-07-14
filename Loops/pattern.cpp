99acebd2ed2e67065e39f60077ce0d507ad3d6cc
istekhar44 <67890istekharalam@gmail.com>
2025-07-14 12:24:39 +0530

    pattern

--- Loops/pattern.cpp
+++ Loops/pattern.cpp
@@ -20,6 +20,11 @@ return 0;
 }*/
 
 
+
+
+
+
+
 /*#include <iostream>
 using namespace std;
 int main(){
@@ -40,18 +45,178 @@ int main(){
 }*/
 
 
-// Make the code of this pattern
+
+
+
+
+
+
+/*// Make the code of this pattern
 
 // 1 2 3 4
 // 1 2 3 4
 // 1 2 3 4
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
     int num ;
-    
+    cin>>num;
+     int row;
+     row = 0;
+     while(row<num){
+        int col;
+        col =1;
+        while(col<=num){
+          cout<<col;
+          col +=1;
+        }
+        cout<<endl;
+        row +=1;
+
+     }
+}*/
+
+
+
+
+
+
+
+
+
+/*   
+
+// print this Pattern:
+//  1   2   3   4   5
+//  6   7   8   9   10
+//  11  12  13  14  15
+
+#include <iostream>
+using namespace std;
+int main(){
+    int num ;
+    num = 0;
+    int count;
+    count = 1;
+    int row ;
+    row = 1;
+    while(row<=num){
+        int col;
+        col=1;
+        while(col<=num){
+            cout << count<< " ";
+            count +=1;
+            col +=1;
+
+        }
+        cout<<endl;
+        row +=1;
+    }
+
+
+
+}
+*/
+
+
+
+
+
+
+
+/*
+//Print the triangle pattern 
+// *
+// * *
+// * * * 
+// * * * *
+#include<iostream>
+using namespace std;
+int main(){
+    int  num;
+    cin>>num;
+    int row= 1;
+    while(row <= num){
+        int col = 1;
+        while(col<=row){
+            cout<<"*";
+            col +=1;
+            
+
+
+        }
+        cout<<endl;
+        row +=1;
+
+    }
 
    
 return 0;
 }
+
+
+
+*/
+
+
+
+
+
+
+/*
+1
+23
+456
+78910
+1112131415
+161718192021
+
+#include <iostream>
+using namespace std;
+int main(){
+    int num ;
+    cin>>num;
+    int row = 1;
+    int count = 1;
+    while(row<=num){
+        int col = 1;
+        while(col<row){
+            cout<<count <<" ";
+            count +=1;
+            col +=1;
+
+
+        }
+        cout << endl;
+        row +=1;
+
+
+    }
+
+    
+
+}*/
+
+
+
+
+//Character type pattern Question
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int row = 1;
+//     while()
+    
+
+
+// }
+
+
+
+
+
+
 
